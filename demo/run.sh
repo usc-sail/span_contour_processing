33997a23c6893a8e1a9d83a55ebc2e14a5456cce
Tanner Sorensen <tsorense@usc.edu>
2018-03-17 12:55:49 -0700

    deleting old/renamed files, deleting files specific to sorensen2018 paper

--- demo/run.sh
+++ demo/run.sh
@@ -3,3 +3,5 @@
 wget --output-document download.zip http://span.usc.edu/owncloud/index.php/s/JNjdOK5pOE9oyt5/download download.zip
 unzip download.zip
 rm download.zip
+
+matlab -nodesktop ./demo/main.m
